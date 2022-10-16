ec449b832439cab52ed87df6f13ada618c400f36
Andrey Popov <andrej_1987@bk.ru>
2022-10-16 15:54:11 +0500

    Добавьте файлы проекта.

---
+++ EmployerTime/EmployerTime.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+
+namespace Indexers_lab
+{
+    enum DayOfWeek
+    {
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        friday,
+        saturday,
+        sunday
+    }
+    class EmployerTime : ICounter, IWhenCreated, IWorkToDisk
+    {
+        private int[] WorkTime;
+        private int summOfHours;
+        private DateTime CreatedTime;
+        private string Name;
+
+        public DateTime Created
+        {
+            get
+            {
+                Console.WriteLine($"Объект {this.ToString()}  был создан {CreatedTime.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
+                return CreatedTime;
+            }
+        }
+        public int count
+        {
+            get
+            {
+                foreach (var item in WorkTime)
+                {
+                    summOfHours += item;
+                }
+                return summOfHours;
+            }
+        }
+        public void BriefPrint()
+        {
+            foreach (var item in Enum.GetValues(typeof(DayOfWeek)))
+            {
+                Console.WriteLine($"{item} - {WorkTime[(int)item]}");
+            }
+        }
+        public string filename
+        {
+            get
+            {
+                return Name;
+            }
+        }
+        public bool SaveToDisk()
+        {
+            StreamWriter writer = new StreamWriter(filename, true);
+            try
+            {
+                foreach (var item in Enum.GetValues(typeof(DayOfWeek)))
+                {
+                    writer.WriteLine($"{item} - {WorkTime[(int)item]}");
+                }
+                writer.WriteLine($"За неделю отработанно - {count} час(ов/а)\nОбъект {filename}" +
+                    $" был создан { CreatedTime.ToString("yyyy-MM-dd HH:mm:ss.fff")}\n");
+
+                writer.Close(); 
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool ReadFromDisk()
+        {
+            StreamReader reader = new StreamReader(filename);
+            string Text;
+            try
+            {
+                Text = reader.ReadToEnd();
+                Console.Write(Text);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public EmployerTime(string _name)
+        {
+            WorkTime = new int[7];
+            summOfHours = 0;
+            CreatedTime = DateTime.Now;
+            Name = _name;
+        }
+        public int this[int index]
+        {
+            set { WorkTime[index] = value; }
+            get { return WorkTime[index]; }
+        }
+    }
+} 
---
+++ EmployerTime/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Indexers_lab
+{
+
+    interface ICounter
+    {
+        int count { get; }
+        void BriefPrint();
+    }
+    interface IWhenCreated
+    {
+        DateTime Created { get; }
+    }
+    interface IWorkToDisk
+    {
+        string filename { get; }
+        bool SaveToDisk();
+        bool ReadFromDisk();
+    }
+    class Program
+    {
+        public void ProgramOperation()
+        {
+            Console.Write("Введите имя работника: ");
+            string Name = Console.ReadLine();
+            var worker1 = new EmployerTime(Name + ".txt");
+            string hours;
+            int time;
+            bool res;
+            for (int i = 0; i < 7; i++)
+            {
+                bool isCorrect = false;
+                do
+                {
+                    Console.Write("Отработанно в {0} - ", Enum.GetName(typeof(DayOfWeek), i));
+                    hours = Console.ReadLine();
+                    res = int.TryParse(hours, out time);
+                    if (res && time >= 0 && time <= 24)
+                    {
+                        worker1[i] = time;
+                        isCorrect = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Что-то пошло не так. Попробуйте снова (ввод от 0 до 24):");
+                    }
+                } while (isCorrect == false);
+            }
+            Console.WriteLine();
+            //worker1.BriefPrint();
+            if (worker1.SaveToDisk())
+            {
+                Console.WriteLine("Запись прошла успешна.\n");
+            }
+            else
+            {
+                Console.WriteLine("Запись не удалась.\n");
+            }
+
+            if (worker1.ReadFromDisk())
+            {
+                Console.WriteLine("Чтение выполнено успешно.\n");
+            }
+            else
+            {
+                Console.WriteLine("Чтение не удалось.\n");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.ProgramOperation();
+        }
+    }
+}
